ec5a104cf85b9321fae4f9d5a38d152fd86e76ef
Acanezytah <akaneyurin1@gmail.com>
2025-10-05 22:51:11 -0700

    Adición de la tabla funcional junto con imagenes y los videos embed.

--- entities/deseado.py
+++ entities/deseado.py
@@ -1,16 +1,29 @@
-class Animal:
-    def __init__(self, name, color):
+class Deseado:
+    def __init__(self, name, image, link, text, video):
         self.name = name
-        self.color = color
+        self.image = image
+        self.link = link
+        self.text = text
+        self.video = video
     
     @classmethod
     def get_list(cls):
         deseados = [
-            cls("Aguila", "Blanca"),
-            cls("Perrito", "Dorado"),
-            cls("Baboson", "Brown"),
-            cls("Tortuga", "Void"),
-            cls("Lobo", "Dark Gray")
+            cls("DRAGON QUEST III HD-2D Remake", 
+                "https://m.media-amazon.com/images/M/MV5BODg5MzMwMDgtNTVkYy00YTI1LWIwOTAtMDlmOWEwNDliMzUzXkEyXkFqcGc@._V1_.jpg",
+                "https://store.steampowered.com/app/2701660/DRAGON_QUEST_III_HD2D_Remake/",
+                "Lo quiero porque me empecé a jugar los originales y me emociona mucho verlo con tecnología mucho mas moderna y contenido nuevo!!",
+                "https://www.youtube.com/embed/1ltsk99E-RY?si=w92p02v8wlRk1qEv"),
+            cls("Persona 3 Reload",
+                "https://preview.redd.it/the-box-art-for-persona-3-reload-v0-gl0jcz2p9tjb1.jpg?width=640&crop=smart&auto=webp&s=fb6d0b6aae168f87daf7bb52317d1a842ffac421",
+                "https://store.steampowered.com/app/2161700/Persona_3_Reload/",
+                "Lo quiero pq me gustan mucho los personas, pero, no lo comprare hasta que me termine el 5.",
+                "https://www.youtube.com/embed/DG_gy1Tecco?si=12K7pGVTP8--c18s"),
+            cls("Professor Layton and The New World of steam",
+                "https://gamefaqs.gamespot.com/a/box/2/7/5/1131275_front.jpg",
+                "https://www.nintendo.com/es-mx/store/products/professor-layton-and-the-new-world-of-steam-switch/?srsltid=AfmBOory-MuCUegwodRx1ItxsxZH1aPKLmQcy_pDeY9nN9sEBniVY5ct",
+                "Aun no sale pero lo quiero porque me gustan mucho los juegos de puzzles y professor layton es de mis sagas favoritaas!! Amo todo de ellos, en especial la musica.",
+                "https://www.youtube.com/embed/9Sj0BXBNugQ?si=F0QabTEeOTqvjSo2"),
         ]
         return deseados
         
